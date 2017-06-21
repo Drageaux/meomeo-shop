d943f7088aa1d9f90d81b633ec77936741868bcb
David Thong Nguyen <thnguyen2808@gmail.com>
2017-06-21 11:20:38 +0700

    Routing and dynamic title

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,29 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Hello from '@/components/Hello';
+import Home from '@/components/Home';
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/docs',
-      name: 'Docs',
-      component: Hello
-    },
-    {
-      path: '/',
-      name: ''
-    }
-  ]
+let router = new Router({
+    routes: [
+        {
+            path: '/docs',
+            name: 'Docs',
+            component: Hello
+        },
+        {
+            path: '/',
+            name: 'Home',
+            meta: {title: 'Trang Chủ'},
+            component: Home
+        }
+    ]
 })
+
+router.beforeEach((to, from, next) => {
+    window.document.title = to.meta.title ? to.meta.title + ' | Meo Meo Shop' : 'Meo Meo Shop';
+    next()
+})
+
+export default router;
