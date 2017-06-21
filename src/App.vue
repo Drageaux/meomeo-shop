f3a87c85d40796dfba346461b36008706df6cc97
David Thong Nguyen <thnguyen2808@gmail.com>
2017-06-21 14:28:42 +0700

    Basic layout and topnav

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
     <div id="app">
-        <img src="./assets/img/logo.png">
+        <div id="header">
+            <img src="./assets/img/brand.png">
+        </div>
+        <div id="main-nav" class="ui secondary pointing menu">
+            <a class="active item">
+                Áo Gối
+            </a>
+            <a class="item">
+                Quần Đội Đầu
+            </a>
+            <a class="item">
+                Xu Chiêng
+            </a>
+        </div>
         <router-view></router-view>
     </div>
 </template>
@@ -11,13 +24,26 @@
     }
 </script>
 
-<style>
+<style lang="scss" rel="stylesheet/scss">
+    $color_main: #4593D5;
+
     #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        #header {
+            background: $color_main;
+            height: 135px;
+            width: 100%;
+            img {
+                height: 100%;
+                position: relative;
+            }
+        }
+        #main-nav {
+            margin-top: 0;
+            justify-content: center;
+        }
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-        margin-top: 60px;
     }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="hello">
+    <div id="home">
         <h1 class="ui header">{{ msg }}</h1>
     </div>
 </template>
@@ -9,32 +9,17 @@
         name: 'home',
         data () {
             return {
-                msg: 'Welcome to Your Vue.js App'
+                msg: 'Chào mừng đến Ụm Bò Shop'
             }
         },
         mounted: function () {
-            console.log($('.ui.header'));
         }
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-    h1, h2 {
-        font-weight: normal;
-    }
-
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-
-    a {
-        color: #42b983;
+<style lang="scss" rel="stylesheet/scss" scoped>
+    #home {
+        margin-top: 100px;
     }
 </style>
