4d367e49a3ed97a258fcb83e9d21483bd165ea20
David Thong Nguyen <thnguyen2808@gmail.com>
2017-06-23 23:39:24 +0700

    Excellent carousel progress
    Removing curly braces when importing --> import the "export default"
    component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,38 +1,21 @@
 <template>
     <div id="home">
-        <slider :auto=false height="auto" animation="fade" :interval=6000 :indicators=false
-                id="slider"
-                class="ui fluid container">
-            <slider-item v-for="(i, index) in list" :key="index">
-                <img :src="i.src">
-            </slider-item>
-        </slider>
+        <carousel class="ui fluid container"></carousel>
     </div>
 </template>
 
 <script>
-    import {
-        Slider,
-        SliderItem
-    } from 'vue-easy-slider'
+    import carousel from './Home/Carousel.vue'
 
     export default {
         name: 'home',
         data () {
             return {
-                msg: 'Chào mừng đến Ụm Bò Shop',
-                list: [
-                    {src: 'https://trello-attachments.s3.amazonaws.com/5949dfad6fb0a49b1c8db34a/594a1287a67c3268a5e0ce00/9bf36603f0f82f8acab5aa51b28891e7/18700361_631142830407959_8529166212666831486_o.jpg'},
-                    {src: 'https://trello-attachments.s3.amazonaws.com/5949dfad6fb0a49b1c8db34a/594a12a75caf76e066433f3b/6f43ff01885f81c04e3fd21834d611ef/19224939_641151472740428_1997971478293195668_n.jpg'},
-                    {src: 'https://trello-attachments.s3.amazonaws.com/5949dfad6fb0a49b1c8db34a/594a12be50db958b6b663c7a/24856ac0ad12c7f38c1f7baf8b76fa9f/19221669_641627969359445_7550867045889901768_o.jpg'}
-                ]
+                msg: 'Chào mừng đến Ụm Bò Shop'
             }
         },
-        mounted: function () {
-        },
         components: {
-            Slider,
-            SliderItem
+            carousel
         }
     }
 </script>
@@ -40,40 +23,6 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" rel="stylesheet/scss" scoped>
     #home {
-        #slider {
-            .slider-item {
-                .wrap {
-                    width: 100%;
-                    height: 100%;
-                    min-height: 1px;
-                    float: left;
-                    display: block;
-                    position: relative !important;
-                    img {
-                        /**
-                        vertical-align: middle;
-                        height: auto;
-                        position: absolute;
-                        top: 50%;
-                        left: 50%;
-                        transform: translate(-50%, -50%);
-                        object-fit: cover;
-                        */
-                        width: 100%;
-                        max-height: 100%;
-                        margin: 0;
-                        padding: 0;
-                        border: 0;
-                        font-size: 100%;
-                        font: inherit;
-                        vertical-align: baseline;
-                        display: block;
-                    }
-                }
-                position: relative;
-            }
-            margin: 0 !important;
-        }
         padding-bottom: 70px;
     }
 </style>
---
+++ src/components/Home/Carousel.vue
@@ -0,0 +1,85 @@
+<template>
+    <swiper :options="swiperOption" ref="mySwiper"
+            id="carousel">
+        <!-- slides -->
+        <swiper-slide v-for="(i, index) in list" :key="index">
+            <img :src="i.src">
+        </swiper-slide>
+        <!-- optional controls -->
+        <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+    </swiper>
+</template>
+
+<script>
+    import {
+        swiper,
+        swiperSlide
+    } from 'vue-awesome-swiper';
+
+    export default {
+        data () {
+            return {
+                list: [
+                    {src: 'https://trello-attachments.s3.amazonaws.com/5949dfad6fb0a49b1c8db34a/594a1287a67c3268a5e0ce00/9bf36603f0f82f8acab5aa51b28891e7/18700361_631142830407959_8529166212666831486_o.jpg'},
+                    {src: 'https://trello-attachments.s3.amazonaws.com/5949dfad6fb0a49b1c8db34a/594a12a75caf76e066433f3b/6f43ff01885f81c04e3fd21834d611ef/19224939_641151472740428_1997971478293195668_n.jpg'},
+                    {src: 'https://trello-attachments.s3.amazonaws.com/5949dfad6fb0a49b1c8db34a/594a12be50db958b6b663c7a/24856ac0ad12c7f38c1f7baf8b76fa9f/19221669_641627969359445_7550867045889901768_o.jpg'}
+                ],
+                swiperOption: {
+                    // NotNextTick is a component's own property,
+                    // and if notNextTick is set to true,
+                    // the component will not instantiate the swiper through NextTick,
+                    // which means you can get the swiper object the first time
+                    // (if you need to use the get swiper object to do what Things,
+                    // then this property must be true)
+                    notNextTick: true,
+                    // swiper configs
+                    autoplay: 5000,
+                    loop: true,
+                    lazyLoadingInPrevNext: true,
+                    // controls
+                    pagination: '.swiper-pagination',
+                    paginationClickable: true,
+                    prevButton: '.swiper-button-prev',
+                    nextButton: '.swiper-button-next',
+                    // if you need use plugins in the swiper, you can config in here like this
+                    debugger: true,
+                    // swiper callbacks
+                    onTransitionStart (swiper) {
+                        console.log(swiper)
+                    }
+                }
+            }
+        },
+        components: {
+            swiper,
+            swiperSlide
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" rel="stylesheet/scss" scoped>
+    #carousel {
+        .swiper-wrapper {
+            .swiper-slide {
+                // near-perfect responsive image sizing solution
+                img {
+                    max-height: 100%;
+                    display: block;
+                    vertical-align: middle;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    position: absolute;
+                }
+                background-color: #eee;
+                overflow: hidden; // prevent overlaps
+            }
+            position: relative;
+        }
+        margin: 0 !important;
+        height: 60vw;
+    }
+</style>
