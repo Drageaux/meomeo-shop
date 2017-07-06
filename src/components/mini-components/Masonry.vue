db0c0ee50bc494220f3d881a1a47c217474dc9d6
David Thong Nguyen <thnguyen2808@gmail.com>
2017-07-06 14:29:47 +0700

    Working with a few test images

--- src/components/mini-components/Masonry.vue
+++ src/components/mini-components/Masonry.vue
@@ -10,34 +10,29 @@
 </template>
 
 <script>
-    import {masonry} from 'masonry-layout';
+    var Masonry = require('masonry-layout');
+    var imagesLoaded = require('imagesloaded');
+    imagesLoaded.makeJQueryPlugin($);
 
     export default {
         mounted () {
-//            var msnry = new Masonry('.grid', {
-//                transitionDuration: '0.3s',
-//                itemSelector: '.item',
-//                columnWidth: '.sizer',
-//                percentPosition: true
-//            });
-            let $grid = $('#masonry')[0]
-                .masonry({
-                    transitionDuration: '0.3s',
-                    itemSelector: '.item',
-                    columnWidth: '.sizer',
-                    percentPosition: true
-                });
-//            $grid.imagesLoaded().progress(function () {
-//                $grid.masonry('layout');
-//            });
-            console.log($grid)
+            let msnry = new Masonry('#masonry', {
+                transitionDuration: '0.3s',
+                itemSelector: '.item',
+                columnWidth: '.sizer',
+                percentPosition: true
+            });
+
+            $('#masonry').imagesLoaded().progress(function () {
+                msnry.layout();
+            });
         },
         data () {
             return {
                 blocks: [
-                    {src: 'https://trello-attachments.s3.amazonaws.com/5949dfad6fb0a49b1c8db34a/594a1287a67c3268a5e0ce00/9bf36603f0f82f8acab5aa51b28891e7/18700361_631142830407959_8529166212666831486_o.jpg'},
-                    {src: 'https://trello-attachments.s3.amazonaws.com/5949dfad6fb0a49b1c8db34a/594a12a75caf76e066433f3b/6f43ff01885f81c04e3fd21834d611ef/19224939_641151472740428_1997971478293195668_n.jpg'},
-                    {src: 'https://trello-attachments.s3.amazonaws.com/5949dfad6fb0a49b1c8db34a/594a12a75caf76e066433f3b/6f43ff01885f81c04e3fd21834d611ef/19224939_641151472740428_1997971478293195668_n.jpg'},
+                    {src: 'http://placehold.it/240x120&text=240x120'},
+                    {src: 'http://placehold.it/220x420&text=220x420'},
+                    {src: 'http://placehold.it/520x520&text=520x520'},
                     {src: 'https://trello-attachments.s3.amazonaws.com/5949dfad6fb0a49b1c8db34a/594a12a75caf76e066433f3b/6f43ff01885f81c04e3fd21834d611ef/19224939_641151472740428_1997971478293195668_n.jpg'},
                     {src: 'https://trello-attachments.s3.amazonaws.com/5949dfad6fb0a49b1c8db34a/594a12be50db958b6b663c7a/24856ac0ad12c7f38c1f7baf8b76fa9f/19221669_641627969359445_7550867045889901768_o.jpg'},
                     {src: 'https://trello-attachments.s3.amazonaws.com/5949dfad6fb0a49b1c8db34a/594a12be50db958b6b663c7a/24856ac0ad12c7f38c1f7baf8b76fa9f/19221669_641627969359445_7550867045889901768_o.jpg'},
@@ -58,12 +53,17 @@
         .sizer, .item {
             width: 33%;
         }
-        .item--width2 {
-            width: 40%;
-        }
         img {
             width: 100%;
             display: block;
+        }
+    }
+
+    @media only screen and (max-width: 768px) {
+        #masonry {
+            .sizer, .item {
+                width: 50%;
+            }
         }
     }
 </style>
